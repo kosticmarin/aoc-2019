c2c0401d9898c0564e019cd1fa4ca2c2c01b856f
Marin Koštić <kosticmarin@gmail.com>
2019-12-02 23:23:57 +0100

    Implement part 2 od d1

--- src/d1.rs
+++ src/d1.rs
@@ -1,11 +1,20 @@
 use std::fs;
 
+fn fuel_for_fuel(mass: i32) -> i32 {
+    let fuel = mass / 3 - 2;
+    if fuel <= 0 {
+        0
+    } else {
+        fuel + fuel_for_fuel(fuel)
+    }
+}
+
 fn fuel_counter_upper(input: String) -> i32 {
     input
         .split_whitespace()
         .map(|s| s.parse::<i32>())
         .filter_map(Result::ok)
-        .map(|n| (n / 3) - 2)
+        .map(fuel_for_fuel)
         .sum()
 }
 
@@ -20,28 +29,28 @@ mod tests {
 
     #[test]
     fn test_fuel_counter_upper_12() {
-        assert_eq!(2i32, fuel_counter_upper("12".to_owned()));
+        assert_eq!(2, fuel_counter_upper("12".to_owned()));
     }
 
     #[test]
     fn test_fuel_counter_upper_14() {
-        assert_eq!(2i32, fuel_counter_upper("14".to_owned()));
+        assert_eq!(2, fuel_counter_upper("14".to_owned()));
     }
 
     #[test]
     fn test_fuel_counter_upper_1969() {
-        assert_eq!(654i32, fuel_counter_upper("1969".to_owned()));
+        assert_eq!(966, fuel_counter_upper("1969".to_owned()));
     }
 
     #[test]
     fn test_fuel_counter_upper_100756() {
-        assert_eq!(33583i32, fuel_counter_upper("100756".to_owned()));
+        assert_eq!(50346, fuel_counter_upper("100756".to_owned()));
     }
 
     #[test]
     fn test_fuel_counter_upper_sum_of_examples() {
         let input = "12\n14\n1969\n100756\n".to_owned();
-        let expected = 2i32 + 2i32 + 654i32 + 33583i32;
+        let expected = 2 + 2 + 966 + 50346;
         assert_eq!(expected, fuel_counter_upper(input));
     }
 }
